6940e20c83a4ea56dbf2242c674be0d1e232110b
Laura Robison <laurarobison@JoeFuscoJunior.attlocal.net>
2024-05-25 12:20:54 -0400

    2024 updates

--- camp_master.py
+++ camp_master.py
@@ -3,9 +3,29 @@ import os
 import csv
 
 import datetime
+
+# loop column headers and find index?
+STUDENT_CHAPERONE_COLUMN = 3
+REGISTRATION_DATE_COLUMN = 2
+FIRST_NAME_COLUMN = 6
+LAST_NAME_COLUMN = 7
+CHURCH_COLUMN = 32
+SHIRT_SIZE_COLUMN = 17
+GENDER_COLUMN = 16
+STUDENT_CAMP_COLUMN = 4
+CHAPERONE_CAMP_COLUMN = 5
+PAYMENT_COLUMN = 48
+CAMP_COLUMN = 49
+
+CAMPS = {
+   'high_school': [],
+   'middle_school': []
+}
+
+
 hs_output_list = []
 ms_output_list = []
-file_path = './church/2023-06-23/'
+file_path = './church/2024-05-25/'
 for root, dirs, files in os.walk(file_path, topdown=False):
    for name in files:
       print(os.path.join(root, name))
@@ -19,41 +39,44 @@ for root, dirs, files in os.walk(file_path, topdown=False):
       #   print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
       #   print("Cell D30 is {0}".format(sh.cell_value(rowx=2, colx=3)))
       for rx in range(sh.nrows-1):
-        if sh.cell_value(rowx=rx+1, colx=5) == '':
+        # What is this checking
+        if sh.cell_value(rowx=rx+1, colx=LAST_NAME_COLUMN) == '':
            continue
-        given_name = sh.cell_value(rowx=rx+1, colx=4)
-        family_name = sh.cell_value(rowx=rx+1, colx=5)
+        given_name = sh.cell_value(rowx=rx+1, colx=FIRST_NAME_COLUMN)
+        family_name = sh.cell_value(rowx=rx+1, colx=LAST_NAME_COLUMN)
         name = f'{family_name}, {given_name}'
-        church =  sh.cell_value(rowx=rx+1, colx=6)
-        shirt =  sh.cell_value(rowx=rx+1, colx=9)
+        church =  sh.cell_value(rowx=rx+1, colx=CHURCH_COLUMN)
+        shirt =  sh.cell_value(rowx=rx+1, colx=SHIRT_SIZE_COLUMN)
         mr = ''
         tower = ''
-        tf = 1 if sh.cell_value(rowx=rx+1, colx=8) == 'Female' and sh.cell_value(rowx=rx+1, colx=7) == 'Student' else ''
-        tm = 1 if sh.cell_value(rowx=rx+1, colx=8) == 'Male' and sh.cell_value(rowx=rx+1, colx=7) == 'Student' else ''
-        af = 1 if sh.cell_value(rowx=rx+1, colx=8) == 'Female' and sh.cell_value(rowx=rx+1, colx=7) == 'Chaperone' else ''
-        am = 1 if sh.cell_value(rowx=rx+1, colx=8) == 'Male' and sh.cell_value(rowx=rx+1, colx=7) == 'Chaperone' else ''
-        datestr =  sh.cell_value(rowx=rx+1, colx=12)
+        tf = 1 if sh.cell_value(rowx=rx+1, colx=GENDER_COLUMN) == 'Female' and sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Student' else ''
+        tm = 1 if sh.cell_value(rowx=rx+1, colx=GENDER_COLUMN) == 'Male' and sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Student' else ''
+        af = 1 if sh.cell_value(rowx=rx+1, colx=GENDER_COLUMN) == 'Female' and sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Chaperone' else ''
+        am = 1 if sh.cell_value(rowx=rx+1, colx=GENDER_COLUMN) == 'Male' and sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Chaperone' else ''
+        datestr =  sh.cell_value(rowx=rx+1, colx=REGISTRATION_DATE_COLUMN)
         print(datestr)
         date_parse = datetime.datetime.strptime(datestr, '%b %d, %Y')
         print(date_parse)
-        first_date = 1 if sh.cell_value(rowx=rx+1, colx=7) != 'Chaperone' and date_parse <= datetime.datetime(2023, 5, 15) else ''
-        second_date = 1 if sh.cell_value(rowx=rx+1, colx=7) != 'Chaperone' and date_parse > datetime.datetime(2023, 5, 15) and date_parse <= datetime.datetime(2023, 6, 1) else ''
-        late_date = 1 if sh.cell_value(rowx=rx+1, colx=7) != 'Chaperone' and date_parse > datetime.datetime(2023, 6, 1) else ''
-        # late_fee = 250 if date_parse > datetime.datetime(2023, 6, 1) else ''
-        adult_one = 1 if sh.cell_value(rowx=rx+1, colx=7) == 'Chaperone' and sh.cell_value(rowx=rx+1, colx=56).lower() != 'both camps' else ''
-        adult_both = 1 if sh.cell_value(rowx=rx+1, colx=7) == 'Chaperone' and sh.cell_value(rowx=rx+1, colx=56).lower() == 'both camps' else ''
-        pay_form = 'online' if sh.cell_value(rowx=rx+1, colx=10) != '' else ''
+        first_date = 1 if sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) != 'Chaperone' and date_parse <= datetime.datetime(2024, 5, 10) else ''
+        second_date = 1 if sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) != 'Chaperone' and date_parse > datetime.datetime(2024, 5, 24) and date_parse <= datetime.datetime(2023, 6, 1) else ''
+        late_date = 1 if sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) != 'Chaperone' and date_parse > datetime.datetime(2024, 5, 31) else ''
+        late_fee = 25 if date_parse > datetime.datetime(2024, 5, 31) else ''
+        adult_one = 1 if sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Chaperone' and sh.cell_value(rowx=rx+1, colx=CAMP_COLUMN).lower() != 'both camps' else ''
+        adult_both = 1 if sh.cell_value(rowx=rx+1, colx=STUDENT_CHAPERONE_COLUMN) == 'Chaperone' and sh.cell_value(rowx=rx+1, colx=CAMP_COLUMN).lower() == 'both camps' else ''
+        pay_form = 'online' if sh.cell_value(rowx=rx+1, colx=PAYMENT_COLUMN) != '' else ''
         # print(sh.cell_value(rowx=rx+1, colx=6).lower())
-        if sh.cell_value(rowx=rx+1, colx=56).lower() == 'high school camp':
-            hs_output_list.append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, adult_one, adult_both, pay_form))
-        elif sh.cell_value(rowx=rx+1, colx=56).lower() == 'both camps':
-            hs_output_list.append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, adult_one, adult_both, pay_form))
-            ms_output_list.append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, adult_one, adult_both, pay_form))
+        if sh.cell_value(rowx=rx+1, colx=CAMP_COLUMN).lower() == 'high school camp':
+            CAMPS['high_school'].append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, late_fee, adult_one, adult_both, pay_form))
+        elif sh.cell_value(rowx=rx+1, colx=CAMP_COLUMN).lower() == 'both camps':
+            CAMPS['high_school'].append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, late_fee, adult_one, adult_both, pay_form))
+            CAMPS['middle_school'].append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, late_fee, adult_one, adult_both, pay_form))
         else:
-            ms_output_list.append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, adult_one, adult_both, pay_form))
+            CAMPS['middle_school'].append((name, church, shirt, mr, tower, tf, tm, af, am, first_date, second_date, late_date, late_fee, adult_one, adult_both, pay_form))
 
 
-with open('./church/master_sheet.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile, delimiter=',')
-    for i in ms_output_list:
-        spamwriter.writerow(i)
+for camp_group in CAMPS.keys():
+    with open(f'./church/main_roster_sheet_{camp_group}.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',')
+        for i in CAMPS[camp_group]:
+            spamwriter.writerow(i)
+
